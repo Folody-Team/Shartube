73e503ea2e5119a2051bf17642e77cd0ca9d277a
tritranduc11 <tritranduc11@gmail.com>
2022-07-28 16:27:12 +0700

    add docker file and docke-compose
    - fix some graphql gateway error
    
    - move server to comic folder

--- graphql-gateway/index.ts
+++ graphql-gateway/index.ts
@@ -1,13 +1,28 @@
-import { ApolloServer } from 'apollo-server';
-const { ApolloGateway } = require('@apollo/gateway');
-
-
-const gateway = new ApolloGateway();
+import { ApolloServer } from "apollo-server";
+import { ApolloGateway } from "@apollo/gateway";
+import dotenv from "dotenv"
+import path from "path"
+dotenv.config({
+  path: path.join(__dirname,".env")
+})
+const gateway = new ApolloGateway({
+  serviceList: [
+    {
+      name: "comic",
+      url: process.env.COMIC_SERVER_HOST,
+    },
+  ],
+});
 
 const server = new ApolloServer({
   gateway,
 });
 
-server.listen(2100).then(({ url }) => {
-  console.log(`🚀 Gateway ready at ${url}`);
-}).catch(err => {console.error(err)});
+server
+  .listen(process.env.PORT)
+  .then(({ url }) => {
+    console.log(`🚀 Gateway ready at ${url}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
